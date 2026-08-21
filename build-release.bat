26c643190a27bdf534fc6c600bd5ed0e48bb9654
Tran Hoang Phuc <tv.hoangphuc@gmail.com>
2026-08-21 13:57:33 +0700

    Add build and build-release batch scripts

---
+++ build-release.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Building XA in Release mode...
+where cargo >nul 2>nul
+if %errorlevel% equ 0 (
+    cargo build --release
+) else (
+    if exist "%USERPROFILE%\.cargo\bin\cargo.exe" (
+        "%USERPROFILE%\.cargo\bin\cargo.exe" build --release
+    ) else (
+        echo [ERROR] Cargo was not found in your PATH or under %USERPROFILE%\.cargo\bin\cargo.exe.
+        echo Please ensure Rust is installed correctly.
+        pause
+        exit /b 1
+    )
+)
+
+if %errorlevel% equ 0 (
+    echo [SUCCESS] Build release version succeeded!
+) else (
+    echo [ERROR] Build failed.
+)
+pause
---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Building XA in Debug mode...
+where cargo >nul 2>nul
+if %errorlevel% equ 0 (
+    cargo build
+) else (
+    if exist "%USERPROFILE%\.cargo\bin\cargo.exe" (
+        "%USERPROFILE%\.cargo\bin\cargo.exe" build
+    ) else (
+        echo [ERROR] Cargo was not found in your PATH or under %USERPROFILE%\.cargo\bin\cargo.exe.
+        echo Please ensure Rust is installed correctly.
+        pause
+        exit /b 1
+    )
+)
+
+if %errorlevel% equ 0 (
+    echo [SUCCESS] Build debug version succeeded!
+) else (
+    echo [ERROR] Build failed.
+)
+pause
